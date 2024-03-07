84f7d3496f86dd50472a7970175cedc4a62e1c1d
Briseyda Villasana Villanueva <101473027+briseydavillasana@users.noreply.github.com>
2024-03-06 18:30:34 -0600

    Add files via upload

---
+++ demo1/src/main/java/com/appwithspring/demo1/controllers/UserController.java
@@ -0,0 +1,12 @@
+package com.appwithspring.demo1.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class UserController {
+    @GetMapping("details")
+public String details(){
+    return"details";
+}
+}
